280d46b626bc6c258d662215d2e52f8605563e68
sahilsk <sahilsk@sahilsk-VirtualBox.(none)>
2014-07-03 12:44:12 +0530

    added dev deps for functional test

---
+++ test/index.js
@@ -0,0 +1,22 @@
+// Use webdriverjs to create a Selenium Client
+var client = require('webdriverjs').remote({
+    desiredCapabilities: {
+        // You may choose other browsers
+        // http://code.google.com/p/selenium/wiki/DesiredCapabilities
+        browserName: 'phantomjs'
+    },
+    // webdriverjs has a lot of output which is generally useless
+    // However, if anything goes wrong, remove this to see more details
+    logLevel: 'silent'
+});
+ 
+client.init();
+
+
+client.url('http://example.com/')
+client.getTitle(function(title){
+    console.log('Title is', title);
+});
+/*client.setValue('#field', 'value');
+client.submitForm()*/;
+client.end();
